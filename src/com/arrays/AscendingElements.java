8b1cac76641c8830c05e572ff04b554ae2c8d880
gitanjali-13 <gitaanjalide@gmail.com>
2022-10-18 09:02:27 +0530

    ascending descending

--- src/com/arrays/AscendingElements.java
+++ src/com/arrays/AscendingElements.java
@@ -2,25 +2,26 @@ package com.arrays;
 
 public class AscendingElements {
     public static void main(String[] args) {
-        int[] arr = {2,3,4,6,9,5};
+        int[] arr = {2, 3, 4, 6, 9, 5};
         int temp;
-        System.out.println("origignal array :");
-        for (int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]+ " ");
+        System.out.print("origignal array :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        for (int i = 0; i < arr.length; i++){
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[i] > arr[j]){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) { //for decending change arr[i] < arr[j]
                     temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
                 }
             }
         }
-        System.out.println("");
-        System.out.println("after ascending order" );
-        for (int i = 0; i < arr.length; i ++){
-            System.out.println(arr[i] +" ");
+        System.out.println();
+        System.out.print("after ascending order");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
+
